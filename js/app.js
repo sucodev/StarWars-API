7a64209eca6b8ee3d0657e8efc331cd130158062
Marcos Del Valle <agenciatomz@gmail.com>
2019-07-15 04:55:17 -0300

    Modificação do app.js e exclusão de arquivos desnecessários

--- js/app.js
+++ js/app.js
@@ -1,111 +1,81 @@
-document.addEventListener('DOMContentLoaded', randomPlanets);
+document.onreadystatechange = function(){
+    if(document.readyState == "complete"){
+        $('#next').click(function(){
+            showPlanets();
+        });
+    }
+}
 
+// ********** Configuração dos parâmetros **********
 
-function randomPlanets(){
-        var id = randomId(61,1); // paginas random de 1 a 61
-        var request = new XMLHttpRequest();
-        var URL = 'https://swapi.co/api/planets/' + id;
+function showPlanets(){
+        // ********** Variaveis relacionadas aos resultados dos planetas **********
+        let planetName = document.querySelector('#planetName'); // Seleciona o campo #planetName
+        let population = document.querySelector('#population'); // Seleciona o campo #population
+        let climate = document.querySelector('#climate'); // Seleciona o campo #climate
+        let terrain = document.querySelector('#terrain'); // Seleciona o campo #terrain
 
-        request.open('GET', URL, true);
-        request.onload = function(){
-            if (request.status >= 200 && request.status < 400) {
-                var response = JSON.parse(request.responseText);
+        // ********** Variaveis relacionadas aos resultados dos filmes **********
+        let numberMovies = document.querySelector('#movies'); // Seleciona o campo #movies
+        let showMovies = document.querySelector('#featuredFilms'); // Seleciona o campo #featuredFilms
+        // ********** Variaveis relacionadas aos resultados da requisição **********
+        let idPlanet = randomId(61,1); // Função responsável pelo id do planeta
+        let request = new XMLHttpRequest; // Criamos o objeto XMLHttpRequest
+        let URL = 'https://swapi.co/api/planets/' + idPlanet; // Concatenação da URL com o id do planeta
 
-                // // Recebe o numero da pagina
-                // let planetPage = document.querySelector('#planetPage');
-                // planetPage.innerHTML = 'Page id = ' + id;
+        // ********** Configuração dos parâmetros **********
+        request.onreadystatechange = function(){
+            if(request.readyState === 4){
+                if(request.status == 200){
+                    response = JSON.parse(request.responseText);
+                    console.log(response);
 
-              // Recebe o nome do planeta
-                const planetName = document.querySelector('#planetName');
-                planetName.innerHTML = response.name;
+                    planetName.innerHTML = response.name; // Imprimir a resposta no campo #planetName
+                    typeWritter(planetName);
+                    // ********** Recebe a população **********
+                    population.innerHTML = response.population; // Imprimir a resposta no campo #population
+                    typeWritter(population);
+                    // ********** Recebe o clima **********
+                    climate.innerHTML = '  ' + response.climate; // Imprimir a resposta no campo #climate
+                    typeWritter(climate);
+                    // ********** Recebe o terreno **********
+                    terrain.innerHTML = '  ' + response.terrain; // Imprimir a resposta no campo #terrain
+                    typeWritter(terrain);
 
-                typeWritter(planetName);
+                    // ********** Recebe a quantidade de filmes **********
+                    let moviesCount = response.films.length;
+                    if(moviesCount == 1){
+                        // Vai imprimir na tela a quantidade de filmes no singular caso a contagem seja igual a 1
+                        numberMovies.innerHTML = '  ' + moviesCount  + ' film ';
+                        }else{
+                        // Vai imprimir na tela a quantidade de filmes no plural caso seja a contagem seja maior que 1
+                        numberMovies.innerHTML = '  ' + moviesCount + ' films';
+                    }
 
-                // Recebe a população
-                const population = document.querySelector('#population');
-                population.innerHTML = response.population;
+                    var lista = document.querySelector('#featuredFilms');
+                    lista.innerHTML = "";
 
-                typeWritter(population);
+                    for(var e = 0; e < response.films.length ; e++){ // Verifica se a quantidade filmes é maior do que zero
+                        (function(idFilms){
+                            const films = response.films[idFilms]; // Essa variavel armazena o endereço : 'https://swapi.co/films/' + o id dos filmes dentro de um array
+                            const newUrlHost = films;
+                            const newRequest = new XMLHttpRequest(); // Cria um novo objeto XMLHTTPRequest
+                            newRequest.open('GET', newUrlHost, true); // Configuração dos parâmetros - Método GET, URL e conexão assíncrona
+                            newRequest.onload = function newRequestStatus() {
+                                    var newResponse =  JSON.parse(newRequest.responseText);
 
-                // Recebe o clima
-                const climate = document.querySelector('#climate');
-                climate.innerHTML = '  ' + response.climate;
+                                    const moviesList = document.createElement('li'); // Criamos um novo elemento li ( para ser exibido como lista )
+                                    moviesList.textContent = newResponse.title;
+                                    showMovies.appendChild(moviesList); // Conecta o elemento filho ( movieList ) no elemento pai ( showMovies )
 
-                typeWritter(climate);
-
-                // Recebe o terreno
-                const terrain = document.querySelector('#terrain');
-                terrain.innerHTML = '  ' + response.terrain;
-
-                // Retorna a quantidade de filmes
-                const movies = document.querySelector('#movies');
-                const films = response.films.length;
-                    movies.innerHTML = '  ' + films + ' films ';
-
-                if(films == 1){
-                    filmsList = movies.innerHTML = '  ' + films  + ' film ';
-                }else{
-                    filmList = movies.innerHTML = '  ' + films + ' films';
+                            };
+                            newRequest.send(newRequest.abort());  // Enviando as requisições para o servidor
+                        })(e);
                     }
                 }
-            typeWritter(movies);
-        };
-        request.send();
-    }
-
-    $( "#next" ).click(function() {
-        var id = randomId(61,1); // paginas random de 1 a 61
-        var request = new XMLHttpRequest();
-        var URL = 'https://swapi.co/api/planets/' + id;
+            }
+        }
 
         request.open('GET', URL, true);
-        request.onload = function(){
-            if (request.status >= 200 && request.status < 400) {
-                var response = JSON.parse(request.responseText);
-
-                // // Recebe o numero da pagina
-                // let planetPage = document.querySelector('.hud');
-                // planetPage.innerHTML = 'Page id = ' + id;
-
-                // Recebe o nome do planeta
-                const planetName = document.querySelector('#planetName');
-                planetName.innerHTML = response.name;
-
-                typeWritter(planetName);
-
-                // Recebe a população
-                const population = document.querySelector('#population');
-                population.innerHTML = response.population;
-
-                typeWritter(population);
-
-                // Recebe o clima
-                const climate = document.querySelector('#climate');
-                climate.innerHTML = response.climate;
-
-                typeWritter(climate);
-
-                // Recebe o terreno
-                const terrain = document.querySelector('#terrain');
-                terrain.innerHTML = '  ' + response.terrain;
-
-                typeWritter(terrain);
-
-                // Retorna a quantidade de filmes
-                const movies = document.querySelector('#movies');
-                const films = response.films.length;
-
-                // let filmsList = movies.innerHTML = 'Movies: ' + films + ' films';
-                if(films == 1){
-                    const filmsList = movies.innerHTML = + ' ' + films  + ' film';
-                }else{
-                    const filmsList = movies.innerHTML = + ' ' + films + ' films';
-                    }
-                }
-                typeWritter(movies);
-        };
         request.send();
     }
-    );
-
-
